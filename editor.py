bde09f83cbd11f6c6294fc0a9a63a3f03fd4988d
Mode <mode89@mail.ru>
2017-03-07 22:15:02 +0800

    create Screen in main loop of Editor

--- editor.py
+++ editor.py
@@ -1,4 +1,12 @@
+import screen
+
 class Editor:
+
+    def run(self):
+        try:
+            self.screen = screen.Screen()
+        finally:
+            self.screen.release()
 
     def open(self, file_name):
         with open(file_name, "r") as f:
