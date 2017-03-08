af4bc28fd584106fa1c596c7dabeca7402e777eb
Mode <mode89@mail.ru>
2017-03-08 17:48:23 +0800

    implement command line wrapper

--- cli.py
+++ cli.py
@@ -1,4 +1,22 @@
+import curses
 from editor import *
 
-editor = Editor()
-editor.run()
+def wrappred_main(screen):
+    curses.use_default_colors()
+    curses.noecho()
+    curses.cbreak()
+    curses.raw()
+    screen.keypad(True)
+
+    editor = Editor()
+    while not editor.exiting:
+        screen.clear()
+        screen.addstr(editor.screen.content())
+        editor.input_buffer.put(screen.getch())
+        editor.refresh()
+
+def main():
+    curses.wrapper(wrappred_main)
+
+if __name__ == "__main__":
+    main()
