dced96cce26696c72ee4b45189e45132a94fd335
Mode <mode89@mail.ru>
2017-03-10 13:51:16 +0800

    read file content into buffer

--- buffer.py
+++ buffer.py
@@ -2,3 +2,7 @@ class Buffer:
 
     def __init__(self, file_name):
         self.file_name = file_name
+        with open(file_name, "r") as f:
+            self.lines = []
+            for line in f:
+                self.lines.append(line)
